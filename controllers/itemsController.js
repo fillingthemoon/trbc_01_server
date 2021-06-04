d56b6bc4b01c0f594930006f5f4bafd8090eac98
philemonheng <phil.hsy@outlook.com>
2021-06-04 14:06:18 +0800

    update itemsController

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -30,6 +30,7 @@ itemsRouter.get('/item/:id/:langId', async (request, response) => {
   response.json(item)
 })
 
+// Can just use English for this
 itemsRouter.get('/pages/', async (request, response) => {
   const items = await Enitem
     .aggregate([
@@ -77,45 +78,5 @@ itemsRouter.get('/pages/', async (request, response) => {
     ])
   response.json(items)
 })
-
-// itemsRouter.post('/', middleware.userExtractor, async (request, response) => {
-//   const body = request.body
-//   const user = request.user
-
-//   const blog = new Blog({
-//     title: body.title,
-//     author: body.author,
-//     url: body.url,
-//     likes: body.likes ? body.likes : 0,
-//     user: user._id
-//   })
-
-//   const savedBlog = await blog.save()
-
-//   user.blogs = user.blogs.concat(savedBlog)
-//   await user.save()
-
-//   response.status(201).json(savedBlog)
-// })
-
-// itemsRouter.put('/:id', async (request, response) => {
-//   const blog = request.body
-
-//   const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-//   response.json(result)
-// })
-
-// // Needs to be in an async function because findByIdAndRemove returns a Promise
-// servicesRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
-//   const user = request.user
-
-//   const blog = await Blog.findById(request.params.id)
-//   if ((blog.user.toString() === user.id.toString())) {
-//     await Blog.findByIdAndRemove(request.params.id)
-//     response.status(204).send() // Correct user: blog removed
-//   } else {
-//     response.status(204).send() // Incorrect user: Blog not removed
-//   }
-// })
 
 module.exports = itemsRouter
